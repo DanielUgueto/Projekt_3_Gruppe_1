b8dad4cf7b73fc5f6938247e4eb7aba199e19579
LatinaEnjoyer <nioe1000@stud.ek.dk>
2026-05-04 13:35:39 +0200

    Model created

---
+++ src/main/java/dk/ek/bilabonnement2026/model/EmployeeModel.java
@@ -0,0 +1,19 @@
+package dk.ek.bilabonnement2026.model;
+
+public class EmployeeModel {
+    int employeeId;
+    String firstName;
+    String lastName;
+    String password;
+    String workEmail;
+    String role;
+
+    public EmployeeModel(int employeeId, String firstName, String lastName, String password, String workEmail, String role) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.workEmail = workEmail;
+        this.role = role;
+    }
+}
