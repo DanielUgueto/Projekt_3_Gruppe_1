1a977a20d7d6483f66c76261ef2261218f016477
RwR92 <ruro0001@stud.ek.dk>
2026-05-04 15:54:46 +0200

    made createCar and showCreateCarForm in CarController class and added a new cunstructor and removed setters in the Car class

--- src/main/java/dk/ek/bilabonnement2026/controller/CarController.java
+++ src/main/java/dk/ek/bilabonnement2026/controller/CarController.java
@@ -1,6 +1,41 @@
 package dk.ek.bilabonnement2026.controller;
 
+import dk.ek.bilabonnement2026.model.Car;
+import dk.ek.bilabonnement2026.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class CarController {
 
-    //hej
+    @Autowired
+    CarService carService;
+
+    @GetMapping("/cars/create")
+    public String showCreateCarForm() {
+        return "createCar";
+    }
+
+    @PostMapping("/cars/create")
+    public String createCar(@RequestParam("carModelId") int carModelId,
+                            @RequestParam("vinNumber") String vinNumber,
+                            @RequestParam("licensePlate") String licensePlate,
+                            @RequestParam("colour") String colour,
+                            @RequestParam("status") String status,
+                            @RequestParam("monthlyPrice") double monthlyPrice,
+                            Model model){
+        Car car = new Car(carModelId,vinNumber,licensePlate,monthlyPrice,status,colour);
+
+        try{
+            carService.createCar(car);
+            return "redirect:/cars";
+        }catch (IllegalArgumentException e){
+            model.addAttribute("fejl", e.getMessage());
+            return "createCar";
+        }
+    }
 }
--- src/main/java/dk/ek/bilabonnement2026/model/Car.java
+++ src/main/java/dk/ek/bilabonnement2026/model/Car.java
@@ -21,60 +21,43 @@ public class Car {
         this.status = status;
         this.colour = colour;
     }
+    public Car(int carModelId, String vinNumber, String licensePlate,
+               double monthlyPrice, String status, String colour){
+        this.carId = carId;
+        this.carModelId = carModelId;
+        this.vinNumber = vinNumber;
+        this.licensePlate = licensePlate;
+        this.monthlyPrice = monthlyPrice;
+        this.status = status;
+        this.colour = colour;
+    }
 
     public int getCarId() {
         return carId;
-    }
-
-    public void setCarId(int carId) {
-        this.carId = carId;
     }
 
     public int getCarModelId() {
         return carModelId;
     }
 
-    public void setCarModelId(int carModelId) {
-        this.carModelId = carModelId;
-    }
-
     public String getVinNumber() {
         return vinNumber;
-    }
-
-    public void setVinNumber(String vinNumber) {
-        this.vinNumber = vinNumber;
     }
 
     public String getLicensePlate() {
         return licensePlate;
     }
 
-    public void setLicensePlate(String licensePlate) {
-        this.licensePlate = licensePlate;
-    }
-
     public String getColour() {
         return colour;
-    }
-
-    public void setColour(String colour) {
-        this.colour = colour;
     }
 
     public String getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
     public double getMonthlyPrice() {
         return monthlyPrice;
     }
 
-    public void setMonthlyPrice(double monthlyPrice) {
-        this.monthlyPrice = monthlyPrice;
-    }
 }
