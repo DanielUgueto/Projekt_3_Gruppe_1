a9a41d4aa5dc9a23b7217d8862c4c9390e895d3b
RwR92 <ruro0001@stud.ek.dk>
2026-05-07 12:20:10 +0200

    Added a missing method in RentalContractRepository called findRentalContractById

--- src/main/java/dk/ek/bilabonnement2026/model/Customer.java
+++ src/main/java/dk/ek/bilabonnement2026/model/Customer.java
@@ -18,7 +18,7 @@ public class Customer {
         this.email = email;
         this.phoneNumber = phoneNumber;
     }
-    public Customer( String firstName, String lastName, String driversLicenseNumber, String cprNumber, String email, int phoneNumber) {
+    public Customer( String firstName, String lastName, int driversLicenseNumber, String cprNumber, String email, int phoneNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.driversLicenseNumber = driversLicenseNumber;
