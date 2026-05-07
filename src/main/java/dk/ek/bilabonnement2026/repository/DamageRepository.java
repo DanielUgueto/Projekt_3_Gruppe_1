62d749602c15be1f85eb44cd0e3e616668820d95
RwR92 <ruro0001@stud.ek.dk>
2026-05-07 12:44:05 +0200

    Made DamageRepository with save method

---
+++ src/main/java/dk/ek/bilabonnement2026/repository/DamageRepository.java
@@ -0,0 +1,33 @@
+package dk.ek.bilabonnement2026.repository;
+
+import dk.ek.bilabonnement2026.model.Damage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@Repository
+public class DamageRepository {
+
+    @Autowired
+    DataSource dataSource;
+
+    public void save(Damage damage){
+        String sql = "INSERT INTO damage (damage_report_id, damage_category_id) VALUES (?, ?)";
+
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)){
+
+            statement.setInt(1,damage.getDamageReportId());
+            statement.setInt(2,damage.getDamageCategoryId());
+
+            statement.executeUpdate();
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
