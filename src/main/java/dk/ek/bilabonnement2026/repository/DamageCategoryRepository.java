3f2d9ad617ce29de590ebc2dd3ea95b47a80cf0a
RwR92 <ruro0001@stud.ek.dk>
2026-05-07 12:50:45 +0200

    Made DamageCategoryRepository with getAllCategories method

---
+++ src/main/java/dk/ek/bilabonnement2026/repository/DamageCategoryRepository.java
@@ -0,0 +1,41 @@
+package dk.ek.bilabonnement2026.repository;
+
+import dk.ek.bilabonnement2026.model.DamageCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class DamageCategoryRepository {
+
+    @Autowired
+    DataSource dataSource;
+
+    public List<DamageCategory> getAllDamageCategories(){
+        List<DamageCategory> categoryList = new ArrayList<>();
+        String sql = "SELECT * FROM damage_category";
+
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery()){
+
+            while(resultSet.next()){
+                categoryList.add(new DamageCategory(resultSet.getInt("damage_category_id"),
+                        resultSet.getString("name"),
+                        resultSet.getDouble("standard_price"),
+                        resultSet.getString("description")));
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return categoryList;
+    }
+}
