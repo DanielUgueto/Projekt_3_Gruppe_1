6791a8afffefc1cafb01e911cf8c1bcde5b155eb
RwR92 <ruro0001@stud.ek.dk>
2026-05-18 14:48:29 +0200

    Added save, update and find methods in DamageCategoryRepository

--- src/main/java/dk/ek/bilabonnement2026/repository/DamageCategoryRepository.java
+++ src/main/java/dk/ek/bilabonnement2026/repository/DamageCategoryRepository.java
@@ -18,9 +18,67 @@ public class DamageCategoryRepository {
     @Autowired
     DataSource dataSource;
 
+    public void saveDamageCategory(DamageCategory category){
+        String sql = "INSERT INTO damage_category (name, standard_price, description)" + "VALUES (?,?,?)";
+
+        try(Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql)){
+
+            statement.setString(1, category.getName());
+            statement.setDouble(2, category.getStandardPrice());
+            statement.setString(3, category.getDescription());
+
+            statement.executeUpdate();
+
+        }catch (SQLException e){
+            throw new RuntimeException("Skadekategorien kunne ikke gemmes i databasen", e);
+        }
+    }
+
+    public void updateDamageCategory(DamageCategory category){
+        String sql = """
+                UPDATE damage_category
+                SET name = ?, standard_price = ?, description = ?
+                WHERE damage_category_id = ?
+                """;
+
+        try(Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql)){
+
+            statement.setString(1, category.getName());
+            statement.setDouble(2, category.getStandardPrice());
+            statement.setString(3, category.getDescription());
+            statement.setInt(4, category.getDamageCategoryId());
+
+            statement.executeUpdate();
+
+        }catch (SQLException e){
+            throw new RuntimeException("Skadekategorien kunne ikke opdateres i databasen", e);
+        }
+    }
+
+    public void updateDamageCategoryIsActive(int damageCategoryId, boolean isActive){
+        String sql = """
+                UPDATE damage_category 
+                SET is_active = ? 
+                WHERE damage_category_id = ?
+                """;
+
+        try(Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql)){
+            statement.setBoolean(1,isActive);
+            statement.setInt(2,damageCategoryId);
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw  new RuntimeException("Skadekategoriens status kunne ikke opdateres i databasen", e);
+        }
+    }
+
     public List<DamageCategory> getAllDamageCategories() {
         List<DamageCategory> categoryList = new ArrayList<>();
-        String sql = "SELECT * FROM damage_category";
+        String sql = "SELECT * FROM damage_category WHERE is_active = true";
 
         try (Connection connection = dataSource.getConnection();
              PreparedStatement statement = connection.prepareStatement(sql);
@@ -28,9 +86,10 @@ public class DamageCategoryRepository {
 
             while (resultSet.next()) {
                 categoryList.add(new DamageCategory(resultSet.getInt("damage_category_id"),
-                        resultSet.getString("name"),
-                        resultSet.getDouble("standard_price"),
-                        resultSet.getString("description")));
+                                resultSet.getString("name"),
+                                resultSet.getDouble("standard_price"),
+                                resultSet.getString("description"),
+                        resultSet.getBoolean("is_active")));
             }
 
         } catch (SQLException e) {
@@ -53,10 +112,35 @@ public class DamageCategoryRepository {
                     return new DamageCategory(resultSet.getInt("damage_category_id"),
                             resultSet.getString("name"),
                             resultSet.getDouble("standard_price"),
-                            resultSet.getString("description"));
+                            resultSet.getString("description"),
+                            resultSet.getBoolean("is_active"));
                 }
             }
         } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public DamageCategory findDamageCategoryByName(String name){
+        String sql = "SELECT * FROM damage_category WHERE name = ?";
+
+        try(Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql)){
+
+            statement.setString(1,name);
+
+            try(ResultSet resultSet = statement.executeQuery()){
+                if(resultSet.next()){
+                    return new DamageCategory(resultSet.getInt("damage_category_id"),
+                    resultSet.getString("name"),
+                    resultSet.getDouble("standard_price"),
+                    resultSet.getString("description"),
+                    resultSet.getBoolean("is_active")
+                    );
+                }
+            }
+        }catch (SQLException e){
             e.printStackTrace();
         }
         return null;
